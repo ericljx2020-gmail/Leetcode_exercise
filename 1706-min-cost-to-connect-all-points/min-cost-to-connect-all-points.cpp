9e798d4437049de2a415d2ab7a7136c5cf823ede
Eric Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2023-09-14 22:40:32 -0700

    Time: 154 ms (82.34%) | Memory: 15.2 MB (90.22%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -1,37 +1,46 @@
 class Solution {
 public:
     static const int N = 1e3+5;
-    struct node{
-        int x,y,w;
-    }edges[N*N];
-    static bool cmp(node a, node b){
-        return a.w < b.w;
-    }
-    int f[N];
-    int find(int x){
-        if (f[x] == x) return x;
-        return f[x] = find(f[x]);
+    int g[N][N], dist[N];
+    unordered_map<int, bool> st;
+    int n;
+    bool flag = 0;
+    int res = 0;
+    void prim(){
+        memset(dist, 0x3f, sizeof dist);
+
+        for (int i = 0; i < n; i++) {
+            int t = -1;
+            for (int j = 0; j < n; j++){
+                if (!st[j] && (t == -1 || dist[t] > dist[j])) t = j;
+            }
+            if (i && dist[t] == 0x3f3f3f3f) {
+                flag = 1;
+                return;
+            }
+
+            if (i){
+                res += dist[t];
+            }
+
+            for (int j = 0; j < n; j++){
+                dist[j] = min(dist[j], g[t][j]);
+            }
+            st[t] = 1;
+        }
     }
     int minCostConnectPoints(vector<vector<int>>& points) {
-        int n = points.size();
+        n = points.size();
         int idx = 0;
+        memset(g, 0x3f, sizeof g);
         for (int i = 0; i < n; i++){
             for (int j = 0; j < n; j++){
                 int a = points[i][0], b = points[i][1], c = points[j][0], d = points[j][1];
-                edges[idx++] = {i, j, abs(a-c) + abs(b-d)};
+                if (i == j) continue;
+                g[i][j] = g[j][i] = min(g[i][j], abs(a-c) + abs(b-d));
             }
         }
-        sort(edges, edges+idx, cmp);
-        int res = 0;
-        for (int i = 0; i < n; i++) f[i] = i;
-        for (int i = 0; i < idx; i++){
-            int x = edges[i].x, y = edges[i].y, w = edges[i].w;
-            int fx = find(x), fy = find(y);
-            if (fx != fy){
-                f[fx] = fy;
-                res += w;
-            }
-        }
+        prim();
         return res;
     }
 };
