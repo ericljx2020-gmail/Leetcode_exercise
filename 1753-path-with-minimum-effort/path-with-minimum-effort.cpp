53cdb147d5acdef2fd2396b318a4c1fc490eafd2
Eric Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2023-09-18 15:33:37 -0700

    Time: 456 ms (8.76%) | Memory: 54.7 MB (9.04%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -1,29 +1,32 @@
-#define pii pair<int, pair<int,int>>
-
+typedef vector<int> VI;
 class Solution {
 public:
-    //Directions (top, right, bottom, left)
-    const int d4x[4] = {-1,0,1,0}, d4y[4] = {0,1,0,-1};
-    
-    int minimumEffortPath(vector<vector<int>>& h) {
-        int n = h.size(), m = h[0].size();
-        priority_queue <pii, vector<pii>, greater<pii>> pq;
-        vector<vector<int>> dis(n, vector<int>(m, INT_MAX));
-        dis[0][0] = 0;
-        pq.push({0, {0, 0}});
-        
-        while(!pq.empty()) {
-            pii curr = pq.top(); pq.pop();
-            int d = curr.first, r = curr.second.first, c = curr.second.second;
-            if(r==n-1 && c==m-1) return d;
-            for(int i=0; i<4; ++i) {
-                int nx = r + d4x[i], ny = c + d4y[i];
-                if(nx < 0 || nx >= n || ny < 0 || ny >= m)continue;
-                int nd = max(d, abs(h[nx][ny] - h[r][c]));
-                if (nd < dis[nx][ny]) {
-                    dis[nx][ny] = nd;
-                    pq.push({nd, {nx, ny}});
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        int n = heights.size(), m = heights[0].size();
+        int dx[4] = {0,0,1,-1};
+        int dy[4] = {1,-1,0,0};
+        unordered_map<int, unordered_map<int,bool>> st;
+        priority_queue<VI, vector<VI>, greater<VI>> q;
+        q.push({0,0,0});
+        int dif[n][m];
+        memset(dif, 0x3f, sizeof dif);
+
+        while (q.size()){
+            VI t = q.top();
+            q.pop();
+            st[t[1]][t[2]] = 1;
+            if (t[1] == n-1 && t[2] == m-1) return t[0];
+            for (int i = 0; i < 4; i++){
+                int a = t[1] + dx[i], b = t[2] + dy[i];
+                if (a < 0 || a >= n || b < 0 || b >= m) continue;
+                if (st[a][b]) continue;
+
+                int cur = max(t[0], abs(heights[a][b] - heights[t[1]][t[2]]));
+                if (cur < dif[a][b]){
+                    q.push({cur, a, b});
+                    dif[a][b] = cur;
                 }
+                // cout << cur << " " << a << " " << b << "\n";
             }
         }
         return 0;
