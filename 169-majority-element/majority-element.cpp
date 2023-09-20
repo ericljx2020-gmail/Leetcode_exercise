491feb1c23360d9abe4396c620e0e877aff9e4ba
Eric Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2023-09-19 19:56:18 -0700

    Time: 15 ms (55.68%) | Memory: 20.1 MB (5.25%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int,int>hash;
+        for (int i = 0; i < n; i++){
+            hash[nums[i]]++;
+            if (hash[nums[i]] > n / 2) return nums[i];
+        }
+        return 0;
+    }
+};
