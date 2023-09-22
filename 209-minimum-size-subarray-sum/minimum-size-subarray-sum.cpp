619ac12a6a459fe9d4dc5bfca2ed1854849d1711
Eric Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2023-09-22 10:38:15 -0700

    Time: 35 ms (23.54%) | Memory: 29.2 MB (5.73%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -1,30 +1,40 @@
 class Solution {
 public:
+    static const int N = 1e5+5;
+    int res = INT_MAX;
+
+    bool check(int k, int target, vector<int>& nums){
+        int q[N];
+        int tt = -1, hh = 0;
+        int crsum = 0;
+        
+        for (int i = 0; i < nums.size(); i++){
+            if (tt >= hh && i-k+1 > q[hh]){
+                crsum -= nums[q[hh]];
+                hh++;
+            }
+            q[++tt] = i;
+            crsum += nums[i];
+            if (crsum >= target) return true;
+            if (i >= k-1 && crsum >= target){
+                res = min(res, crsum);
+            }
+        }
+        return false;
+    }
+
     int minSubArrayLen(int target, vector<int>& nums) {
         int n = nums.size();
-        nums.insert(nums.begin(),0);
-        vector<int> f(n+1, 0);
-        for (int i = 1; i <= n; i++){
-            f[i] = f[i-1] + nums[i];
-            cout << f[i] << " ";
-        }
-        puts("");
-        int res = INT_MAX;
-        for (int i = 1; i <= n; i++){
-            int l = i, r = n;
-            while (l < r){
-                int mid = l + r >> 1;
-                if (f[mid]-f[i-1] >= target){
-                    r = mid;
-                }else{
-                    l = mid+1;
-                }
+        int l = 1, r = n;
+        while (l < r){
+            int mid = l + r >> 1;
+            if (check(mid, target, nums)){
+                r = mid;
+            }else{
+                l = mid+1;
             }
-            cout << r;
-            if (f[r] - f[i-1] < target) continue;
-            res = min(res, r-i+1);
         }
-        if (res == INT_MAX) res = 0;
-        return res;
+        if (check(r, target, nums)) return r;
+        return 0;
     }
 };
