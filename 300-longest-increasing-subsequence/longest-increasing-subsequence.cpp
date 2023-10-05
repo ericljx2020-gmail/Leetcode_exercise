e8ed02534673384af344372047607c62f04eb0ce
Eric Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2023-10-05 14:19:33 -0700

    Time: 195 ms (68.64%) | Memory: 10.9 MB (42.91%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -3,18 +3,18 @@ public:
     static const int N = 2500;
     int lengthOfLIS(vector<int>& nums) {
         int f[N];
+        for (int i = 0; i < nums.size(); i++){
+            f[i] = 1;
+        }
+
         int n = nums.size();
-        for (int i = 0; i < n; i++) f[i] = 1;
+        int res = 1;
         for (int i = 0; i < n; i++){
             for (int j = 0; j < i; j++){
-                if (nums[j] < nums[i]){
-                    f[i] = max(f[i], f[j]+1);
-                }
+                if (nums[j] >= nums[i]) continue;
+                f[i] = max(f[j]+1, f[i]);
+                res = max(f[i], res);
             }
-        }
-        int res = 0;
-        for (int i = 0; i < n; i++){
-            res = max(res, f[i]);
         }
         return res;
     }
