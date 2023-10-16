12bd641659aa8527c86ce1520f33a23efa955c10
Eric Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2023-10-16 12:13:57 -0700

    Time: 134 ms (5.04%) | Memory: 292.8 MB (5.05%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
@@ -2,49 +2,21 @@ class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
         int n = nums.size();
-        int q[500005];
-        int tt = -1;
-        int p[n+1], s[n+1];
+        vector<int> q(500005,0);
+        int len = 0;
         for (int i = 0; i < n; i++){
-            while (tt != -1 && nums[i] > nums[q[tt]]){
-                p[q[tt]] = i;
-                tt--;
+            int l = 0, r = len;
+            while (l < r){
+                int mid = l + r + 1 >> 1;
+                if (q[mid] >= nums[i]){
+                    r = mid-1;
+                }else{
+                    l = mid;
+                }
             }
-            q[++tt] = i;
+            len = max(len, r+1);
+            q[r+1] = nums[i];
         }
-        while (tt != -1){
-            p[q[tt]] = -1;
-            tt--;
-        }
-        tt = -1;
-        for (int i = n-1; i >= 0; i--){
-            while (tt != -1 && nums[i] < nums[q[tt]]){
-                s[q[tt]] = i;
-                tt--;
-            }
-            q[++tt] = i;
-        }
-
-        while (tt != -1){
-            s[q[tt]] = -1;
-            tt--;
-        }
-
-        // for (int i = 0; i < n; i++){
-        //     cout << p[i] << " ";
-        // }
-        // puts("");
-        // for (int i = 0; i < n; i++){
-        //     cout << s[i] << " ";
-        // }
-
-
-        for (int i = 0; i < n; i++){
-            if (p[i] != -1 && s[i] != -1){
-                return true;
-            }
-        }
-
-        return false;
+        return len >= 3;
     }
 };
