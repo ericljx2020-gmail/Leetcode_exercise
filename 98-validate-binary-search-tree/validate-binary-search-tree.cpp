a835becde3daabda38ae21209ebccdae53ab5255
Eric Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2023-11-12 15:52:37 -0800

    Time: 8 ms (68.55%) | Memory: 22.3 MB (15.11%) - LeetSync

---
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public: 
+    vector<int> v;
+
+    void dfs(TreeNode* root){
+        if (root -> left){
+            dfs(root -> left);
+        }
+        v.push_back(root -> val);
+        if (root -> right){
+            dfs(root -> right);
+        }
+        return;
+    }
+
+    bool isValidBST(TreeNode* root) {
+       dfs(root);
+       for (int i = 0; i < v.size(); i++){
+           if (i != 0 && v[i] <= v[i-1]) return false;
+       }
+       return true;
+    }
+};
