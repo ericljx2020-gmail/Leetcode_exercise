2544c2cbf031ba75e8c3626d15675bcdd831c237
Eric Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-02-20 10:27:41 -0800

    Time: 15 ms (30.21%) | Memory: 23.1 MB (14.05%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,44 +1,45 @@
 class Solution {
 public:
-    static const int N = 2e4+4;
-    int trap(vector<int>& h) {
-        int n = h.size();
-        vector<int> asc(n, 0), dec(n,n);
+    int trap(vector<int>& a) {
+        int n = a.size();
+        vector<int> r(n,n), l(n,-1);
+        int q[n+1];
         int tt = -1;
-        int q[N];
         for (int i = 0; i < n; i++){
-            while (tt != -1 && h[q[tt]] <= h[i]){
-                asc[q[tt]] = i;
-                tt--;
+            while (tt != -1 && a[q[tt]] <= a[i]){
+                r[q[tt--]] = i;
             }
             q[++tt] = i;
         }
         tt = -1;
         for (int i = n-1; i >= 0; i--){
-            while (tt != -1 && h[q[tt]] <= h[i]){
-                dec[q[tt]] = i;
-                tt--;
+            while (tt != -1 && a[q[tt]] <= a[i]) {
+                l[q[tt--]] = i;
             }
             q[++tt] = i;
         }
-
+        // for (int i = 0; i < n; i++) cout << r[i] << " ";
+        // puts("");
+        // for (int i = 0; i < n; i++) cout << l[i] << " ";
         int res = 0;
         for (int i = 0; i < n; i++){
-            int r = asc[i];
-            for (int j = i+1; j < r; j++){
-                res += max(0, h[i] - h[j]);
-                h[j] = h[i];
+            if (r[i] != n){
+                for (int j = i+1; j < r[i]; j++) {
+                    res += max(0,a[i]-a[j]);
+                    a[j] = a[i];
+                }
             }
         }
+        // cout << res << endl;
         for (int i = n-1; i >= 0; i--){
-            int l = dec[i];
-            for (int j = i-1; j > l; j--){
-                res += max(0, h[i] - h[j]);
-                h[j] = h[i];
+            if (l[i] != -1){
+                for (int j = i-1; j > l[i]; j--){
+                    res += max(0, a[i]-a[j]);
+                    a[j] = a[i];
+                }
             }
         }
+
         return res;
-        
-        return 0;
     }
 };
