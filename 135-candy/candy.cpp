2697a7b6d336d686bc4b4665e6d046ca53856b6f
Eric Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-02-22 12:56:32 -0800

    Time: 17 ms (47.12%) | Memory: 19.5 MB (54.66%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,36 +1,30 @@
 class Solution {
 public:
-    int candy(vector<int>& ratings) {
-        int asc = 0, dec = -1, dec_max = 1;
-        int res = 0;
-        for (int i = 0; i < ratings.size(); i++){
-            if (i == 0 || ratings[i] > ratings[i-1]) {
-                asc++;
-                res += asc;
-                dec = -1;
-            }else if (ratings[i] < ratings[i-1]){
-                if (dec == -1){
-                    //this is the second of dec sequence, including the last one in asc
-                    dec = 1;
-                    dec_max = asc;
-                }else{
-                    dec++;
-                }
-                if (dec == dec_max){
-                    res = res + dec + 1;
-                    dec_max++;
-                }else{
-                    res += dec;
-                }
-                asc = 1;
+    int candy(vector<int>& a) {
+        int n = a.size();
+        int inc = 1, dec = 1, top = 1;
+        int res = 1;
+        for (int i = 1; i < n; i++){
+            if (a[i] > a[i-1]) {
+                //  uphill
+                inc++;
+                dec = 1;
+                res += inc;
+                top = inc;
+            }else if (a[i] == a[i-1]) {
+                inc = 1;
+                dec = 1;
+                top = 1;
+                res += inc;
             }else{
-                //equal
-                dec_max = 1;
-                asc = 1;
-                dec_max = 1;
-                res += asc;
+                inc = 1;
+                dec++;
+                res += (dec-1);
+                if (dec > top) res ++;
             }
         }
+
         return res;
+
     }
 };
