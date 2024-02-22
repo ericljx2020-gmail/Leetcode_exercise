1e0ad66c263ec01faa41f8c620989a86c6594fc2
Eric Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-02-22 13:22:49 -0800

    Time: 51 ms (96.60%) | Memory: 27.2 MB (56.64%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -3,28 +3,28 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         int n = nums.size();
         sort(nums.begin(), nums.end());
-        int pi = 0x3f3f3f3f;
-        vector<vector<int>> ans;
-        for (int i = 0; i < n; i++){
-            if (pi != 0x3f3f3f3f && pi == nums[i]) continue;
-            pi = nums[i];
-            int pj = 0x3f3f3f3f;
-            for (int j = i+1; j < n; j++){
-                if (pj != 0x3f3f3f3f && pj == nums[j]) continue;
-                pj = nums[j];
-                int target = 0-nums[i]-nums[j];
-                int l = j+1, r = n-1;
-                while (l < r){
-                    int mid = l + r + 1 >> 1;
-                    if (nums[mid] <= target){
-                        l = mid;
-                    }else{
-                        r = mid-1;
-                    }
+        vector<vector<int>> res;
+        for (int i = 0; i < n; i++) {
+            int t = nums[i];
+            if (i != 0 && nums[i] == nums[i-1]) continue;
+            int l = i+1, r = n-1;
+            while (l < r) {
+                int sum = t + nums[l] + nums[r];
+                if (sum < 0){
+                    l++;
+                }else if (sum > 0){
+                    r--;
+                } else{
+                    int sec = nums[l];
+                    int trd = nums[r];
+                    res.push_back({t, nums[l], nums[r]});
+
+                    while (l < r && nums[l] == sec) l++;
+                    while (l < r && nums[r] == trd) r--;
                 }
-                if (r != j && nums[r] == target) ans.push_back({nums[i], nums[j], nums[r]});
             }
         }
-        return ans;
+
+        return res;
     }
 };
