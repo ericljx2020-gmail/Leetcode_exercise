7075685cc38f64afcbb4e2f45acd9e8df44f4c5c
Eric Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-02-23 11:20:00 -0800

    Time: 24 ms (24.25%) | Memory: 26.3 MB (5.29%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        unordered_map<int, unordered_map<int, bool>> row, col, grid;
         int n = board.size();
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < n; j++){
+        unordered_map<int, unordered_map<int, bool>> rol, col, g;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 if (board[i][j] == '.') continue;
-                int g_r = i / 3, g_c = j / 3;
                 int cur = board[i][j] - '0';
-                int oneD = g_r * 3 + g_c;
-                if (grid[oneD][cur] || row[i][cur] || col[j][cur]) return false;
-                grid[oneD][cur] = row[i][cur] = col[j][cur] = 1;
+                int oneD = i / 3 * 3 + j / 3;
+                if (g[oneD][cur] || rol[i][cur] || col[j][cur]) {
+                    cout << i << " " << j << " " << oneD << endl;
+                    return false; 
+                }
+                g[oneD][cur] = rol[i][cur] = col[j][cur] = 1;
             }
         }
         return true;
