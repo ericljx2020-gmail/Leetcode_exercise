54d357fc5cb300b1a1f95d40e45eae13fdeeead6
Eric Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-02-23 20:00:56 -0800

    Time: 11 ms (19.93%) | Memory: 17.6 MB (17.96%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        vector<vector<int>> f(n+5, vector<int> (2, 0));
-        f[0][0] = 0;             //第一天手里没货的价钱
-        f[0][1] = -prices[0];    //第一天手里有货的profit
+        int n= prices.size();
+        vector<vector<int>> dp(n, vector<int>(2, 0));
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
 
         for (int i = 1; i < n; i++){
-            f[i][0] = max(f[i-1][0], f[i-1][1] + prices[i]);
-            f[i][1] = max(f[i-1][0] - prices[i], f[i-1][1]);
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i]);
+            dp[i][1] = max(dp[i-1][1], dp[i][0]-prices[i]);
         }
-
-        return max(f[n-1][0], f[n-1][1]);
+        return dp[n-1][0];
     }
 };
