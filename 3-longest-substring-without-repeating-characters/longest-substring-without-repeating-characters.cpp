a1bc843b0786378c2ebface17a1e731bfaa2d28c
Eric Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-03-05 19:59:24 -0800

    Time: 92 ms (10.40%) | Memory: 11.2 MB (57.61%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,42 +1,21 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int n = s.size();
-        int l = 1, r = n;
-        while (l < r){
-            int mid = l + r + 1 >> 1;
-            cout << mid << "\n";
-            if (check(mid,s)){
-                //当前窗口size可以那么可以试一下更长的
-                l = mid;
-                cout << "OK" << "\n";
+        int res = 0, n = s.size();
+        int l = 0, r = -1;
+        unordered_map<char, char> hash;
+        while (++r < n) {
+            if (hash[s[r]]){
+                while(hash[s[r]]){
+                    hash[s[l++]] = 0;
+                }
+                hash[s[r]] = 1;
             }else{
-                r = mid-1;
-                cout << "Not" << "\n";
+                hash[s[r]] = 1;
             }
-            
+            res = max(res, r-l+1);
+            cout << l << " " << r << " " << res << '\n';
         }
-
-        return r;
-    }
-
-    bool check(int k, string s) {
-        int q[50004];
-        int tt = -1, hh = 0;
-        int n = s.size();
-        unordered_map<char, bool> hash;
-        for (int i = 0; i < n; i++){
-            if (hh <= tt && q[hh] < i-k+1){
-                hash[s[q[hh++]]] = 0;
-            }
-            while (hh <= tt && hash[s[i]]){
-                hash[s[q[hh++]]] = 0;
-            }
-            q[++tt] = i;
-            hash[s[i]] = 1;
-            if (tt - hh + 1 >= k) return true;
-        }
-
-        return false;
+        return res;
     }
 };
