78c3b52d5469e6847368049d7328327a70b6553e
Eric Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-03-05 19:45:29 -0800

    Time: 49 ms (13.17%) | Memory: 18 MB (10.57%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,49 +1,42 @@
 class Solution {
 public:
-
-    bool check(int k, string s){ 
-        unordered_map<char, bool> hash;
-        int n = s.size();
-        int tt = -1, hh = 0;
-        int q[100005];
-        for (int i = 0; i < n; i++) {
-            if (hh <= tt && q[hh] < i-k+1) {
-                hash[s[q[hh]]] = 0;
-                hh++;
-            }
-
-            while (hh <= tt && hash[s[i]]) {
-                // cout << s[q[hh]] << ' ';
-                hash[s[q[hh++]]] = 0;
-            }            
-            // puts("");
-            q[++tt] = i;
-            hash[s[i]] = 1;
-            // cout << hh << " " << tt << " " << k << "\n";
-            if (tt-hh+1 >= k) return true;
-
-        }
-        return false;
-    }
-
     int lengthOfLongestSubstring(string s) {
-        
         int n = s.size();
         int l = 1, r = n;
-        bool flag = false;
         while (l < r){
             int mid = l + r + 1 >> 1;
-            // puts("");
-            if (check(mid, s)) {
+            cout << mid << "\n";
+            if (check(mid,s)){
+                //当前窗口size可以那么可以试一下更长的
                 l = mid;
-                // cout << mid << " " << "True" << "\n";
+                cout << "OK" << "\n";
             }else{
                 r = mid-1;
-                // cout << mid << " " << "False" << "\n";
+                cout << "Not" << "\n";
             }
+            
         }
 
-
         return r;
+    }
+
+    bool check(int k, string s) {
+        int q[50004];
+        int tt = -1, hh = 0;
+        int n = s.size();
+        unordered_map<char, bool> hash;
+        for (int i = 0; i < n; i++){
+            if (hh <= tt && q[hh] < i-k+1){
+                hash[s[q[hh++]]] = 0;
+            }
+            while (hh <= tt && hash[s[i]]){
+                hash[s[q[hh++]]] = 0;
+            }
+            q[++tt] = i;
+            hash[s[i]] = 1;
+            if (tt - hh + 1 >= k) return true;
+        }
+
+        return false;
     }
 };
