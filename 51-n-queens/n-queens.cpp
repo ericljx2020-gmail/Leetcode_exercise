97bef759d3eddde87e3f37d0b986840fc6a56ff5
Eric Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-03-25 14:40:05 -0700

    Time: 9 ms (30.28%) | Memory: 11.1 MB (36.48%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    unordered_map<int, bool> col, dg, udg;
     vector<string> g;
     vector<vector<string>> ans;
+    unordered_map<int, int> col, dg, udg;
     void dfs(int u, int n){
-        if (u == n){
+        if (u == n) {
             ans.push_back(g);
+            return ;
         }
 
         for (int i = 0; i < n; i++){
-            if (!col[i] && !dg[u+i] && !udg[n+i-u]){
-                col[i] = dg[u+i] = udg[n+i-u] = 1;
+            if (!col[i] && !dg[i+u] && !udg[n+i-u]){
+                col[i] = dg[i+u] = udg[n+i-u] = 1;
                 g[u][i] = 'Q';
-                dfs(u+1, n);
-                col[i] = dg[u+i] = udg[n+i-u] = 0;
+                dfs(u+1,n);
                 g[u][i] = '.';
+                col[i] = dg[i+u] = udg[n+i-u] = 0;
             }
         }
     }
@@ -22,8 +23,13 @@ public:
     vector<vector<string>> solveNQueens(int n) {
         string s = "";
         for (int i = 0; i < n; i++) s += '.';
-        for (int i = 0; i < n; i++) g.push_back(s);
-        dfs(0, n);
+
+        for (int i = 0; i < n; i++){
+            g.push_back(s);
+        }
+
+        dfs(0,n);
+
         return ans;
     }
 };
