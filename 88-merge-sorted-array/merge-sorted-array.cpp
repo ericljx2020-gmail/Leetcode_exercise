ba12b0ed81af0300345173a25ca68befd8a31bc3
Eric Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-03-28 14:58:07 -0700

    Time: 0 ms (100.00%) | Memory: 10.9 MB (72.80%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int l = 0, r = 0;
         vector<int> ans;
-        while (l < m || r < n) {
-            if (l >= m) {
-                ans.push_back(nums2[r]); 
-                r++; 
-                continue;
-            }
-            if (r >= n) {
-                ans.push_back(nums1[l]);
-                l++;
-                continue;
-            }
-
-            if (nums1[l] <= nums2[r]) {
-                ans.push_back(nums1[l]);
-                l++;
-            }else{
+        int l = 0, r = 0;
+        while (l < m || r < n){
+            if (l >= m){
                 ans.push_back(nums2[r]);
                 r++;
+            }else if (r >= n){
+                ans.push_back(nums1[l]);
+                l++;
+            }else if (nums1[l] >= nums2[r]){
+                ans.push_back(nums2[r]);
+                r++;
+            }else if (nums1[l] < nums2[r]){
+                ans.push_back(nums1[l]);
+                l++;
             }
         }
         nums1 = ans;
