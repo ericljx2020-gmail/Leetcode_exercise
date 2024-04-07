2ab64a2e9b24849a28141454d0f9ad84e6e3bb80
Eric Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-04-06 20:58:17 -0700

    Time: 1840 ms (5.06%) | Memory: 76.3 MB (10.53%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        unordered_map<int, int> hash, vis;
+        unordered_map<int, int> hash;
         int n = nums.size();
         int res = 0;
         for (int i = 0; i < n; i++){
@@ -9,17 +9,10 @@ public:
         }
         queue<int> q;
         for (int i = 0; i < n; i++){
-            if (!vis[nums[i]]){
+            if (!hash[nums[i]-1]){
                 int cur = 1;
-                vis[nums[i]] = 1;
-                int l = nums[i]-1, r = nums[i]+1;
-                while (l >= 0 && hash[l]){
-                    vis[l] = 1;
-                    cur++;
-                    l--;
-                }
-                while (r < n && hash[r]){
-                    vis[r] = 1;
+                int r = nums[i]+1;
+                while (hash[r]){
                     cur++;
                     r++;
                 }
