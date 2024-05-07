e714a667952f3b73a05ca3f3aabe01d91cdc9bb6
Eric Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-05-06 18:06:38 -0700

    Time: 3 ms (72.32%) | Memory: 8.6 MB (84.19%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
+    /*
+     * 思路：分段，保证每一段的 （ 数量小于 ）数量。
+     */
     int longestValidParentheses(string s) {
         int n = s.size();
         stack<int> stk;
         int res = 0;
+        // ()((()...不关怎么加只要满足左括号数量小于右括号一定能够完全匹配，所以其实每一段的长度是确定的，加入右括号stk还不为空的情况是给结尾准备的
         for (int i = 0, start = -1; i < n; i++){
             if (s[i] == '(') {
                 stk.push(i);
