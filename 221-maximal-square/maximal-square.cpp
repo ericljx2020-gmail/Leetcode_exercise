6722550d8ee0481dcef696aa266230d18543c9b0
Eric Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-05-08 21:29:52 -0700

    Time: 56 ms (84.63%) | Memory: 24.3 MB (81.07%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -2,21 +2,24 @@ class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
         int n = matrix.size(), m = matrix[0].size();
-        int f[n+1][m+1];
-        memset(f, 0, sizeof f);
+        vector<vector<int>> f(n+1, vector<int> (m+1,0));
         int res = 0;
-        for (int i = 0; i < n; i++) f[i][0] = matrix[i][0] - '0', res = max(res, f[i][0]);
-        for (int i = 0; i < m; i++) f[0][i] = matrix[0][i] - '0', res = max(res, f[0][i]);
-        for (int i = 1; i < n; i++){
-            for (int j = 1; j < m; j++){
-                if (matrix[i][j] == '1'){
-                    f[i][j] = min(f[i-1][j], min(f[i][j-1], f[i-1][j-1])) + 1;
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                if (i == 0 || j == 0) {
+                    f[i][j] = (int)(matrix[i][j] == '1');
                     res = max(res, f[i][j]);
+                    continue;
                 }
-                
+                if (matrix[i][j] == '1') f[i][j] = min(f[i-1][j]+1, min(f[i][j-1]+1, f[i-1][j-1]+ 1));
+                else{
+                    f[i][j] = 0;
+                }
+                res = max(res, f[i][j]);
+
             }
         }
-        
+
         return res * res;
     }
 };
