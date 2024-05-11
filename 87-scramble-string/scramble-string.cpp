8ef2059bbcfd37e3ff42c426413f2dac60b422c5
Eric Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-05-10 19:49:25 -0700

    Time: 38 ms (37.45%) | Memory: 13.1 MB (88.05%) - LeetSync

---
+++ 87-scramble-string/scramble-string.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isScramble(string s1, string s2) {
+        int n = s1.size();
+        vector<vector<vector<bool>>> f(n, vector<vector<bool>> (n, vector<bool> (n+1)));
+        for (int k = 1; k <= n; k++) {
+            for (int i = 0; i+k-1 < n; i++) {
+                for (int j = 0; j+k-1 < n; j++) {
+                    if (k == 1) f[i][j][k] = (s1[i] == s2[j]);
+                    else{
+                        for (int u = 1; u < k; u++) {
+                            if (f[i][j][u] && f[i+u][j+u][k-u] || f[i][j+k-u][u] && f[i+u][j][k-u]){
+                                f[i][j][k] = 1;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return f[0][0][n];
+    }
+};
