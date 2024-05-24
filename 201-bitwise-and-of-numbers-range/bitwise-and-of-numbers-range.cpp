b9ff26189f656a96292ae701d56ae96c90a8d7a9
Eric Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-05-24 13:21:55 -0700

    Time: 12 ms (22.53%) | Memory: 8.8 MB (78.81%) - LeetSync

--- 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.cpp
+++ 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
     int rangeBitwiseAnd(int left, int right) {
-        if (left == 0 && right == 0) return 0;
-        if (left == right) return left;
-        int lf = left, rt = right;
-        int l = 0, r = 0;
-        while (left || right){
-            if (left) l++;
-            if (right) r++;
-            left >>= 1;
-            right >>= 1;
+        int hl = 0, hr = 0;
+        for (int i = 0; i < 32; i++){
+            if (left >> i & 1) hl = max(hl, i);
+            if (right >> i & 1) hr = max(hr, i);
         }
-        // cout << l << " " << r;
-        if (l != r) return 0;
-        int mark = 0;
-        for (int i = l-1; i >= 0; i--){
-            if ((lf >> i) != (rt >> i)) {
-                mark = i+1;
-                break;
-            }
+        if (hl != hr) return 0;
+        cout << hl << " " << hr;
+        int res = 0;
+        for (int i = 31; i >= 0; i--) {
+            int c = left >> i & 1;
+            if (c != (right >> i & 1)) break;
+            res += c << i;
         }
-        cout << mark << endl;
-        int mask = (1 << mark)-1;
-        cout << mask;
-        mask = ~mask;
-        return lf & mask;
-
+        return res;
     }
 };
+
+// 0101
+// 0110
+// 0111
+// 1000
