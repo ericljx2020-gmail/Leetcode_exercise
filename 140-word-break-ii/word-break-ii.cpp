d92cefad7694082a39f7fce922217bae06511f99
Eric Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-05-24 20:48:36 -0700

    Time: 0 ms (100.00%) | Memory: 9.4 MB (32.97%) - LeetSync

--- 140-word-break-ii/word-break-ii.cpp
+++ 140-word-break-ii/word-break-ii.cpp
@@ -1,41 +1,39 @@
 class Solution {
 public:
-    unordered_map<string, bool> hash;
-    int n;
     vector<bool> f;
+    unordered_map<string, bool> hash;
     vector<string> res;
     vector<string> wordBreak(string s, vector<string>& wordDict) {
-        n = s.size();
+        int n = s.size();
         f.resize(n+1);
         f[n] = 1;
-
-        for (auto w : wordDict){
+        //f[i]指的是s[i,n]能否组成
+        for (auto w : wordDict) {
             hash[w] = 1;
         }
-
-        for (int i = n-1; i != 0; i--) {
-            for (int j = i; j < n; j++) {
+        for (int i = n-1; i >= 0; i--) {
+            for (int j = n-1; j >= i; j--) {
                 if (f[j+1] && hash[s.substr(i, j-i+1)]){
-                    f[i] = 1;
+                    f[i] = true;
+                    break;
                 }
             }
         }
+        // for (int i = 0; i < n; i++) cout << f[i] << " " ;
 
         dfs(s, 0, "");
         return res;
     }
 
     void dfs(string s, int u, string path) {
-        if (u == n){
-            path.pop_back();            //去掉多余空格
+        if (u == s.size()) {
+            path.pop_back();
             res.push_back(path);
             return;
         }
 
-         for (int i = u; i < n; i++) {
-            if (hash[s.substr(u, i-u+1)] && f[i+1]){
-                dfs(s, i+1, path + s.substr(u, i-u+1) + " ");
-            }
-         }
+        for (int i = u; i < s.size(); i++) {
+            if (hash[s.substr(u, i-u+1)] && f[i+1]) dfs(s, i+1, path+s.substr(u,i-u+1)+" ");
+        }
     }
 };
