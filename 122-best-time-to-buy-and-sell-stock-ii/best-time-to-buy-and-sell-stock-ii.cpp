a2c3dd6056460c77f4700d0bf9e6f9d07957b0c7
Eric Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-05-26 00:25:07 -0700

    Time: 5 ms (55.14%) | Memory: 15.6 MB (28.02%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        prices.insert(prices.begin(), 0);
-        vector<vector<int>> f(n+1, vector<int> (2, 0));
-        
-        f[1][0] = 0;
-        f[1][1] = -prices[1];
-        for (int i = 2; i <= n; i++) {
-            f[i][0] = max(f[i-1][0], f[i-1][1] + prices[i]);
-            f[i][1] = max(f[i-1][0] - prices[i], f[i-1][1]);
+        int res = 0;
+        for (int i = 1; i < prices.size(); i++){
+            res += max(0, prices[i] - prices[i-1]);
         }
-        return f[n][0];
+        return res;
     }
 };
