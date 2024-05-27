42c357a58a12f55aa819d6ea5ea15ee0da52f769
Eric Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-05-26 17:45:26 -0700

    Time: 9 ms (56.76%) | Memory: 11.3 MB (73.57%) - LeetSync

--- 2546-number-of-subarrays-with-gcd-equal-to-k/number-of-subarrays-with-gcd-equal-to-k.cpp
+++ 2546-number-of-subarrays-with-gcd-equal-to-k/number-of-subarrays-with-gcd-equal-to-k.cpp
@@ -5,6 +5,7 @@ public:
         int res = 0;
         for (int i = 0; i < n; i++) {
             int g = 0;
+            //gcd只可能越变越小，因为前面的数字能够决定上限，所以只需要暴力枚举碰到一个gcd等于k的区间记录就可以
             for (int j = i; j < n; j++){
                 g = __gcd(g, nums[j]);
                 if (g % k != 0) break;
