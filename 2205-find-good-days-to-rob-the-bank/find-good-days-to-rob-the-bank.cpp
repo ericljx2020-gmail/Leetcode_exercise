7a113094b7cea266de497092ff325298fe8ba7a5
Eric Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-06-02 17:18:06 -0700

    Time: 451 ms (5.04%) | Memory: 92.3 MB (48.74%) - LeetSync

---
+++ 2205-find-good-days-to-rob-the-bank/find-good-days-to-rob-the-bank.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> goodDaysToRobBank(vector<int>& security, int time) {
+        int n = security.size();
+        vector<int> dec(n,0), inc(n,0);
+        for (int i = 0; i < security.size(); i++) {
+            if (i == 0 || security[i] > security[i-1]){
+                dec[i] = 1;
+            }else{
+                dec[i] = dec[i-1]+1;
+            }
+            cout << dec[i] << ' ';
+        }
+        puts("");
+        for (int i = security.size()-1; i >= 0; i--){
+            if (i == security.size()-1 || security[i] > security[i+1]){
+                inc[i] = 1;
+            }else{
+                inc[i] = inc[i+1]+1;
+            }
+        }
+        for (int i = 0; i < n; i++) cout << inc[i] << " ";
+        vector<int> res;
+        for (int i = 0; i < n; i++) {
+            if (i < time || i >= n-time) continue;
+            if (dec[i] >= time+1 && inc[i] >= time+1) res.push_back(i);
+        }
+
+        return res;
+    }
+};
