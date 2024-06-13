b63531efff1b513df2dc0e9511c52b52ee322eef
Eric Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-06-12 22:39:05 -0700

    Time: 114 ms (9.91%) | Memory: 111.8 MB (45.05%) - LeetSync

--- 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.cpp
+++ 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.cpp
@@ -5,9 +5,11 @@ public:
         int res = 0;
         for (int i = 0; i < nums.size(); i++){
             if (nums[i] == 1 && (q.size() % 2 == 1)){
+                //当前已开始是1，且被翻了奇数次，所以现在是0，要烦
                 q.push(i+k-1);
                 res++;
             }else if (nums[i] == 0 && (q.size() % 2 == 0)){
+                //当前开始是0，且被翻了偶数次，现在是0，所以要翻
                 q.push(i+k-1);
                 res++;
             }
