7d79ad097302bc6a02535e91587b76d56f2cb2ce
Eric Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-06-13 22:11:45 -0700

    Time: 107 ms (16.74%) | Memory: 44.8 MB (16.29%) - LeetSync

---
+++ 1424-maximum-candies-you-can-get-from-boxes/maximum-candies-you-can-get-from-boxes.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int maxCandies(vector<int>& status, vector<int>& candies, vector<vector<int>>& keys, vector<vector<int>>& containedBoxes, vector<int>& initialBoxes) {
+        unordered_map<int, bool> has_key, st;
+
+        queue<int> q;
+        for (auto c : initialBoxes){
+            q.push(c);
+            st[c] = 1;
+        }
+        int res = 0;
+        int cnt = 0;
+        while (q.size()){
+            auto t = q.front();
+            q.pop();
+            cout << t << " ";
+            if (has_key[t] || status[t]){
+                res += candies[t];
+                cnt = 0;
+            }else{
+                q.push(t);
+                cnt++;
+                if (cnt >= q.size()) return res;
+                continue;
+            }
+
+            //add keys
+            for (auto k : keys[t]){
+                has_key[k] = 1;
+            }
+
+            //pushbox
+            for (auto b : containedBoxes[t]){
+                if (!st[b]){
+                    q.push(b);
+                    st[b] = 1;
+                }
+            }
+
+        }
+        return res;
+
+    }
+};
