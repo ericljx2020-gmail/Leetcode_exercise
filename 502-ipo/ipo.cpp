386f6287f5f9a57fc8d1fab551349b8aa52788a8
Eric Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-06-15 16:10:35 -0700

    Time: 148 ms (46.73%) | Memory: 85.5 MB (62.81%) - LeetSync

--- 502-ipo/ipo.cpp
+++ 502-ipo/ipo.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
-
-    static bool cmp(pair<int, int>& a, pair<int, int>& b){
-        return a.second < b.second;
-    }
-
     int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
         priority_queue<int> pq;
-        vector<pair<int, int>> v;
-        for (int i = 0; i < profits.size(); i++){
+        vector<pair<int,int>> v;
+        int n = profits.size();
+        for (int i = 0; i < n; i++){
             v.push_back({profits[i], capital[i]});
         }
         sort(v.begin(), v.end(), cmp);
 
+        // int ans = 0;
         int idx = 0;
-        int ans = 0;
-        while (k --){
-            while (idx < profits.size() && w >= v[idx].second){
-                pq.push(v[idx].first);              // if capital needed is less than we have, we push all possible profits into the maxheap
+        while (k--){
+            while (idx < n && v[idx].second <= w){
+                pq.push(v[idx].first);
                 idx++;
             }
-            if (pq.size() == 0) break;
+            if (pq.empty()) return w;
+            // ans += pq.top();
             w += pq.top();
             pq.pop();
         }
         return w;
+
+    }
+
+    static bool cmp(pair<int,int> a, pair<int,int> b){
+        return a.second < b.second;
     }
 };
