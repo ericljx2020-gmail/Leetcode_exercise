15407f8ff9d8a145ebf8e0eac7baf2a3b54e7afc
Eric Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-06-19 13:53:02 -0700

    Time: 41 ms (14.35%) | Memory: 30.7 MB (6.50%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int maxProfit(int k, vector<int>& prices) {
+        //状态机dp，两个状态，1.手中有货；2.手中无货
+        //f[i][j][0]：第i天在做第k次交易的时候手里没货的最大值
+        //f[i][j][1]: 第i天在做j次交易时候手里有货的最大值
+        //j在买入的时候增加因为买入卖出是一次交易
         int n = prices.size();
         prices.insert(prices.begin(), 0);
         vector<vector<vector<int>>> f(n+1, vector<vector<int>> (k+1, vector<int> (2, 0)));
