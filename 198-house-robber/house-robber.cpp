6731a89f35d023fbd49c940dd985a6a9f021eeba
Eric Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-06-25 00:26:00 -0700

    Time: 0 ms (100.00%) | Memory: 9.5 MB (5.04%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -6,8 +6,8 @@ public:
         vector<vector<int>> f(n+1, vector<int> (2, 0));
         for (int i = 1; i <= n; i++){
             f[i][0] = max(f[i-1][1], f[i-1][0]);
-            f[i][1] = f[i-1][0] + nums[i];
+            f[i][1] = max(f[i-1][0]+nums[i], f[i-1][1]);
         }
-        return max(f[n][0], f[n][1]);
+        return max(f[n][1], f[n][0]);
     }
 };
