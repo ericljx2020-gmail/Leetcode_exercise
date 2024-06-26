c5560f5598b13fd3a1acb64d05363edf89434fde
Eric Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-06-26 16:19:36 -0700

    Time: 731 ms (32.22%) | Memory: 34.8 MB (46.67%) - LeetSync

---
+++ 1348-maximum-score-of-spliced-array/maximum-score-of-spliced-array.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maximumsSplicedArray(self, nums1: List[int], nums2: List[int]) -> int:
+        #保证nums1大
+        print(sum(nums1), sum(nums2))
+        if sum(nums1) < sum(nums2):
+            nums1, nums2 = nums2, nums1
+        print(sum(nums1), sum(nums2))
+        diff1 = []
+        diff2 = []
+        for i in range(len(nums1)):
+            diff1.append(nums2[i] - nums1[i])
+        for i in range(len(nums1)):
+            diff2.append(nums1[i] - nums2[i])
+        s1 = 0
+        res1 = 0
+        s2 = 0
+        res2 = 0
+        for i in range(len(diff1)):
+            s1 = max(s1, 0) + diff1[i]
+            res1 = max(res1, s1)
+            s2 = max(s2, 0) + diff2[i]
+            res2 = max(res2, s2)
+
+        return max(sum(nums1)+res1, sum(nums2)+res2)
