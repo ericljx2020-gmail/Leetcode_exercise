ae72f334da6541e0437fc37a0b2eb3bfc15539d4
Eric Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-06-27 15:56:44 +0800

    Time: 88 ms (52.61%) | Memory: 110.5 MB (60.43%) - LeetSync

--- 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.cpp
+++ 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     int minKBitFlips(vector<int>& nums, int k) {
         queue<int> q;
+        int n = nums.size();
         int res = 0;
-        for (int i = 0; i < nums.size(); i++){
-            if (nums[i] == 1 && (q.size() % 2 == 1)){
-                //当前已开始是1，且被翻了奇数次，所以现在是0，要烦
+        for (int i = 0; i < n; i++){
+            if (i > n-k && ((nums[i] == 0 && q.size() % 2 == 0) || (nums[i] == 1 && q.size() & 1))) return -1;
+            if (nums[i] == 0 && q.size() % 2 == 0){
+                //翻
+                res ++;
+                q.push(i+k-1);         //连续k个都被翻了
+            }else if (nums[i] == 1 && q.size() & 1){
+                res ++;
                 q.push(i+k-1);
-                res++;
-            }else if (nums[i] == 0 && (q.size() % 2 == 0)){
-                //当前开始是0，且被翻了偶数次，现在是0，所以要翻
-                q.push(i+k-1);
-                res++;
             }
-            if (q.size() && q.front() <= i) q.pop();
+            if (q.size() && q.front() == i) q.pop();
         }
-        if (q.size()) return -1;
         return res;
     }
 };
