e218ec2c1866f1386dd0ed53cb76fa3ac14b98c7
Eric Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-07-08 13:50:21 +0800

    Time: 4 ms (85.70%) | Memory: 21.5 MB (10.98%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
         nums.erase(unique(nums.begin(), nums.end()), nums.end());
-        for (int i = 0; i < nums.size(); i++) cout << nums[i] << " ";
         return nums.size();
     }
 };
