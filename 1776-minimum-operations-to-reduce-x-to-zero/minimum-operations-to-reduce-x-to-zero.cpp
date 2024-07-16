cfcec37f73f309a05cda08f89670fbfeeee77641
Eric Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-07-16 17:19:29 +0800

    Time: 128 ms (44.69%) | Memory: 111.7 MB (36.73%) - LeetSync

--- 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.cpp
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.cpp
@@ -2,40 +2,21 @@ class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
         int n = nums.size();
-        nums.insert(nums.begin(), 0);
-        vector<int> s1(n+1, 0), s2(n+2,0);
-        int minv = INT_MAX;
-        for (int i = 1; i <= n; i++){
-            s1[i] = s1[i-1] + nums[i];
-            if (s1[i] == x) minv = min(minv, i);
+        for (int i = 0; i < n; i++){
+            nums.push_back(nums[i]);
         }
-        for (int i = n; i >= 1; i--){
-            s2[i] = s2[i+1] + nums[i];
-            if (s2[i] == x) minv = min(minv, n-i+1);
-        }
-
-        for (int i = n; i >= 2; i--){
-            if (s2[i] > x) break;
-            int c = x - s2[i];
-            int pos = find(1,i-1,c, s1);
-            if (s1[pos] == c && pos + n-i+1 < minv){
-                minv = pos+n-i+1;
-                // cout << pos << " " << c << endl;
+        int sum = 0;
+        int res = INT_MAX;
+        for (int i = 0, j = 0; i < 2 * n; i++){
+            sum += nums[i];
+            while (sum > x){
+                sum -= nums[j++];
+            }
+            if (sum == x && !(i < n-1 && j > 0) && !(i < 2*n-1 && j > n)){
+                res = min(res, i-j+1);
             }
         }
-        if (minv == INT_MAX) return -1;
-        return minv;
-    }
-
-    int find(int l, int r, int x, vector<int> & nums) {
-        while (l < r){
-            int mid = l + r >> 1;
-            if (nums[mid] >= x){
-                r = mid;
-            }else{
-                l = mid+1;
-            }
-        }
-        return r;
+        if (res > n) return -1;
+        return res == INT_MAX ? -1 : res;
     }
 };
