1a324dd84fc94b99b9df90e7c1cf1eec54b81038
Eric Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-07-24 13:12:48 +0800

    Time: 186 ms (12.02%) | Memory: 60.4 MB (20.43%) - LeetSync

---
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    long long countFairPairs(vector<int>& nums, int lower, int upper) {
+        long long res = 0;
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < n-1; i++){
+            int a = nums[i];
+            int l_new = lower - a;
+            int r_new = upper - a;
+            int l = i+1, r = n-1;
+            //找右端点
+            while (l < r){
+                int mid = l + r + 1 >> 1;
+                if (nums[mid] <= r_new){
+                    l = mid;
+                }else{
+                    r = mid-1;
+                }
+            }
+            int rr = r;
+            l = i+1, r = n-1;
+            //找左端点
+            while (l < r){
+                int mid = l + r >> 1;
+                if (nums[mid] >= l_new){
+                    r = mid;
+                }else{
+                    l = mid+1;
+                }
+            }
+            int ll = r;
+            if (nums[ll] < l_new || nums[ll] > r_new || nums[rr] < l_new || nums[rr] > r_new) continue;
+            res += rr-ll+1;
+            cout << ll << " " << rr << "\n";
+        }
+        return res;
+    }
+};
+
+
+// 0 1 4 4 5 7
