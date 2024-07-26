2cc724c35fc710486b358e8cc56d29a9d2f44e62
Eric Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-07-26 18:16:48 +0800

    Time: 4 ms (97.93%) | Memory: 21.3 MB (20.92%) - LeetSync

---
+++ 275-h-index-ii/h-index-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n = citations.size();
+        int l = 0, r = n;
+        while (l < r){
+            int mid = l + r + 1 >> 1;
+            cout << mid << " ";
+            if (citations[n-mid] >= mid){
+                l = mid;
+            }else{
+                r = mid-1;
+            }
+        }
+        return r;
+    }
+};
