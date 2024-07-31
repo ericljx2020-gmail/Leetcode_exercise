bcf498461fffd33e8138ed09c7bad8266d4d9259
Eric Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-07-31 17:59:11 +0800

    Time: 61 ms (14.29%) | Memory: 35.2 MB (11.52%) - LeetSync

--- 1002-maximum-width-ramp/maximum-width-ramp.cpp
+++ 1002-maximum-width-ramp/maximum-width-ramp.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int maxWidthRamp(vector<int>& nums) {
-        int n = nums.size();
         vector<int> v;
-        for (int i = 0; i < n; i++) v.push_back(i);
+        int n = nums.size();
+        for (int i = 0; i < n; i++){
+            v.push_back(i);         //index数组
+        }
         sort(v.begin(), v.end(), [&](int a, int b){
             if (nums[a] != nums[b]) return nums[a] < nums[b];
             return a < b;
         });
         int res = 0;
+
         for (int i = 0, j = v[0]; i < n; i++){
-             res = max(res, v[i] - j);
-             j = min(j, v[i]);
+            res = max(res, v[i] - j);
+            j = min(j, v[i]);
         }
         return res;
     }
