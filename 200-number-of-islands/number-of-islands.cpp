e7a62587a8a64d016d3223158daebbf787afb01d
Eric Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-08-02 17:30:00 +0800

    Time: 106 ms (5.26%) | Memory: 42.7 MB (5.04%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,45 +1,39 @@
 class Solution {
 public:
-    vector<vector<char>> g;
-    int n, m;
-    int cnt = 0;
-    unordered_map<int, unordered_map<int,int>> st;
-    int dx[4] = {0,0,-1,1};
+    unordered_map<int, unordered_map<int, bool>> st;
+    int dx[4] = {0,0,1,-1};
     int dy[4] = {1,-1,0,0};
-    void bfs(int x, int y){
-        st[x][y] = 1;
-        queue<pair<int,int>>q;
-        q.push({x,y});
-
-        while (q.size()){
-            pair<int,int> t = q.front();
-            q.pop();
-
-            for (int i = 0; i < 4; i++){
-                int a = t.first+dx[i], b = t.second+dy[i];
-                if (a < 0 || a >= n || b < 0 || b >= m) continue;
-                if (g[a][b] == '0') continue;
-                if (st[a][b]) continue;
-
-                q.push({a,b});
-                st[a][b] = 1;
-            }
-        }
-    }
-
+    int n,m;
     int numIslands(vector<vector<char>>& grid) {
-        g = grid;
-        if (g.size() == 0) return 0;
-        n = g.size(), m = g[0].size();
+        n = grid.size(), m = grid[0].size();
+        int cnt = 0;
         for (int i = 0; i < n; i++){
             for (int j = 0; j < m; j++){
-                if (g[i][j] == '1' && !st[i][j]){
-                    bfs(i,j);
+                if (!st[i][j] && grid[i][j] == '1'){
+                    bfs(i,j,grid);
                     cnt++;
                 }
             }
         }
-
         return cnt;
+    }
+
+    void bfs(int x, int y, vector<vector<char>>& grid){
+        queue<vector<int>> q;
+        q.push({x,y});
+        st[x][y] = 1;
+        while (q.size()){
+            auto t = q.front();
+            q.pop();
+
+            for (int i = 0; i < 4; i++){
+                int a = dx[i] + t[0], b = dy[i] + t[1];
+                if (a < 0 || a >= n || b < 0 || b >= m) continue;
+                if (st[a][b]) continue;
+                if (grid[a][b] == '0') continue;
+                st[a][b] = 1;
+                q.push({a,b});
+            }
+        }
     }
 };
