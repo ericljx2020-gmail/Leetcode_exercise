f4b9a5b3e3d87b1aca8aaa8aab37a6a8b789d2d6
Eric Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-08-15 16:10:26 +0800

    Time: 18 ms (81.77%) | Memory: 25.6 MB (73.56%) - LeetSync

--- 1219-longest-well-performing-interval/longest-well-performing-interval.cpp
+++ 1219-longest-well-performing-interval/longest-well-performing-interval.cpp
@@ -2,27 +2,24 @@ class Solution {
 public:
     int longestWPI(vector<int>& hours) {
         int n = hours.size();
+        vector<int> ss(n+1, 0);
         hours.insert(hours.begin(), 0);
-        vector<int> s(n+1, 0);
-        for (int i = 1; i <= n; i++){
-            s[i] = s[i-1] + (hours[i] > 8 ? 1 : -1);
-        }
-
+        for (int i = 1; i <= n; i++) ss[i] = ss[i-1] + (hours[i] > 8 ? 1 : -1);
+        
         stack<int> stk;
         stk.push(0);
-
-        for (int i = 1; i < n; i++){
-            if (s[i] < s[stk.top()]){
+        for (int i = 1; i <= n; i++){
+            if (ss[i] < ss[stk.top()]){
                 stk.push(i);
             }
         }
-        int ans = 0;
+        int res = 0;
         for (int i = n; i >= 1; i--){
-            while (stk.size() && s[i] > s[stk.top()]) {
-                ans = max(i-stk.top(), ans);
+            while (stk.size() && ss[i] - ss[stk.top()] > 0){
+                res = max(res, i - stk.top());
                 stk.pop();
             }
         }
-        return ans;
+        return res;
     }
 };
