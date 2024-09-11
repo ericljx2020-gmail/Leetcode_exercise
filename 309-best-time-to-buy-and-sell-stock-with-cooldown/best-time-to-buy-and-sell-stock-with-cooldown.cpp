2d39e5ae2a49b1d9f7d2f429b889950db52bf897
Junxi (Eric) Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-09-11 21:13:21 +0800

    Time: 9 ms (18.11%) | Memory: 14.5 MB (68.15%) - LeetSync

---
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> f(n, vector<int> (3, 0));
+        //f[i][0]：手里没票，是今天卖掉的
+        //f[i][1]：手里没票，不是今天卖掉的
+        //f[i][2]：手里有票
+        f[0][0] = 0;
+        f[0][2] = -prices[0];
+        for (int i = 1; i < n; i++){
+            f[i][0] = f[i-1][2] + prices[i];
+            f[i][1] = max(f[i-1][1], f[i-1][0]);
+            f[i][2] = max(f[i-1][2], f[i-1][1] - prices[i]);
+        }
+        return max(f[n-1][0], f[n-1][1]);
+    }
+};
