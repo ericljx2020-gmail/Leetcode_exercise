c9d11e5b0cda265ac778008975e17534d9676469
Junxi (Eric) Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-10-18 11:20:09 -0700

    Time: 48 ms (99.69%) | Memory: 69.4 MB (11.22%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
-
-    int quick_find(vector<int>& nums, int l, int r, int k){
-        if (l == r) return nums[l];
-        int x = nums[l+r >> 1], j = r + 1, i = l - 1;
-        while (i < j){
-            do i++; while (nums[i] > x);
-            do j--; while (nums[j] < x);
-            if (i < j) swap(nums[i], nums[j]);
+    int findKthLargest(vector<int>& nums, int k) {
+        unordered_map<int, int> hash;
+        int minv = INT_MAX, maxv = INT_MIN;
+        for (auto c : nums) {
+            minv = min(minv, c);
+            maxv = max(maxv, c);
+            hash[c] ++;
         }
 
-        if (j < k) return quick_find(nums, j+1, r, k);
-        else return quick_find(nums, l, j, k);
-    }
-    
-    int findKthLargest(vector<int>& nums, int k) {
-        int n = nums.size();
-        return quick_find(nums, 0, n-1 ,k-1);
+        for (int i = maxv; i >= minv; i--){
+            k -= hash[i];
+            if (k <= 0) return i;
+        }
+        return -1;
     }
 };
