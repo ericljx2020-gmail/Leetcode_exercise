f22f867a2be16b12353a99cb552aa2b20fbb57a5
Junxi (Eric) Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-10-21 17:57:53 -0700

    Time: 55 ms (91.48%) | Memory: 52.2 MB (14.47%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -2,16 +2,14 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         int n = nums.size();
-        nums.insert(nums.begin(), 0);
-        vector<int> f(n+1, 0);
-        for (int i = 1; i <= n; i++) f[i] = f[i-1] + nums[i];
+        vector<int> s(n+1, 0);
+        int res = 0;
         unordered_map<int, int> hash;
         hash[0] = 1;
-        int res = 0;
         for (int i = 1; i <= n; i++){
-            int c = f[i] - k;
-            res += hash[c];
-            hash[f[i]]++;
+            s[i] = s[i-1] + nums[i-1];
+            res += hash[s[i]-k]; 
+            hash[s[i]]++;
         }
         return res;
     }
