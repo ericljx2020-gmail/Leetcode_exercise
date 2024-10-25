5d31182662cc2e2d285ef0634899c5ad5497bb2a
Junxi (Eric) Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-10-25 13:46:01 -0700

    Time: 72 ms (85.08%) | Memory: 242.4 MB (11.35%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-
-    int quicksort(vector<int> nums, int l, int r, int k) {
-        if (l == r) return nums[k];
-        int x = nums[l], i = l-1, j = r+1;
-        while (i < j) {
-            do(i++); while (nums[i] > x);
-            do(j--); while (nums[j] < x);
-            if (i < j) swap(nums[i], nums[j]);
-        }
-
-        if (k <= j) return quicksort(nums, l, j, k);
-        else return quicksort(nums, j+1, r, k);
+    int findKthLargest(vector<int>& nums, int k) {
+        return quick_sort(nums, 0, nums.size()-1, k-1);
     }
 
-    int findKthLargest(vector<int>& nums, int k) {
-        return quicksort(nums, 0, nums.size()-1, k-1);
+    int quick_sort(vector<int> nums, int l, int r, int k) {
+        if (l == r) return nums[k];
+        int x = nums[l], i = l-1, j = r+1;
+        while (i < j){
+            do i++; while (nums[i] > x);
+            do j--; while (nums[j] < x);
+            if (i < j) swap(nums[i], nums[j]);
+        }
+        if (k <= j) return quick_sort(nums, l, j, k);
+        else return quick_sort(nums, j+1, r, k);
     }
 };
