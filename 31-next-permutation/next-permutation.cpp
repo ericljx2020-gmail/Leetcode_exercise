57b85b9f4a096ba256bfbccfe2ced1ccdcc37a99
Junxi (Eric) Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-10-24 23:42:53 -0700

    Time: 17 ms (13.72%) | Memory: 14.8 MB (48.61%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int k = nums.size()-1;
-        while (k > 0 && nums[k] <= nums[k-1]) k--;
-        if (k <= 0) {
+        int t = nums.size()-1;
+        while (t > 0 && nums[t] <= nums[t-1]) t--;         //find the position t-1 < t
+        if (t <= 0) {
             reverse(nums.begin(), nums.end());
-        }else{
-            int t = k;
-            while (t < nums.size() && nums[t] > nums[k-1]) t ++;
-            swap(nums[t-1], nums[k-1]);
-            reverse(nums.begin()+k, nums.end());
+            return;
         }
+
+        for (auto c : nums) cout << c << " ";
+        cout << t << " ";
+        int c = t;
+        while (c < nums.size() && nums[c] > nums[t-1]) c++;
+        swap(nums[t-1], nums[c-1]);
+        reverse(nums.begin()+t, nums.end());
+        cout << c;
         return;
     }
 };
