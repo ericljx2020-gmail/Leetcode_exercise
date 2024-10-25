0f8313f27f488c9f7c16e5225885e52cd58ec7f7
Junxi (Eric) Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-10-25 12:58:20 -0700

    Time: 33 ms (81.89%) | Memory: 91.2 MB (5.05%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -1,19 +1,31 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-        int n = nums.size();
-        int o = 0, z = 0;
-        int res = 0;
-        for (int i = 0, j = 0; i < n; i++){
-            if (nums[i] == 0){
-                while (j <= i && z == k){
-                    z -= (nums[j] == 0);
-                    j++;
-                }
-                z++;
+        int l = 0, r = nums.size();
+        while (l < r){
+            int mid = l + r + 1 >> 1;
+            if (check(nums, mid, k)){
+                l = mid;
+            }else{
+                r = mid-1;
             }
-            res = max(res, i-j+1);
+        }        
+        return r;
+    }
+
+    bool check(vector<int> nums, int c, int k) {
+        int q[1000006];
+        int tt = -1, hh = 0;
+        // [1,2,3,4,5,6], c = 3
+        int zeros = 0;
+        for (int i = 0; i < nums.size(); i++){
+            if (hh <= tt && q[hh] < i-c+1) {
+                zeros -= (nums[q[hh++]] == 0);
+            }
+            q[++tt] = i;
+            zeros += (nums[i] == 0);
+            if (i >= c-1 && zeros <= k) return true;
         }
-        return res;
+        return false;
     }
 };
