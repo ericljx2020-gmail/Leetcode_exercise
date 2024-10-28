8e2dc6b6b2ff4e70c93128dab70d1cea7c9a131c
Junxi (Eric) Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-10-28 13:47:13 -0700

    Time: 22 ms (16.75%) | Memory: 19.5 MB (16.94%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_map<string, bool> hash;
         int n = s.size();
-        s = ' ' + s;
-        vector<bool> f(n+1, 0);
-        for (int i = 0; i < wordDict.size(); i++){
-            hash[wordDict[i]] = 1;
+        vector<int> f(n+1, 0);
+        s = " " + s;
+        f[0] = 1;
+        unordered_map<string, bool> hash;
+        for (auto w : wordDict){
+            hash[w] = 1;
         }
 
-        f[0] = 1;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                string sub = s.substr(j, i-j+1);
-                if (f[j-1] && hash[sub]) {
-                    f[i] = true;
-                    break;
-                }
+        for (int i = 1; i <= n; i++){
+            for (int j = 1; j <= i; j++){
+                f[i] |= f[j-1] && hash[s.substr(j, i-j+1)];
             }
         }
         return f[n];
