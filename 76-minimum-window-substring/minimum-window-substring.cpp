5be9b26e09dd89a099290eff2bddecbd766cf87c
Junxi (Eric) Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-10-28 13:34:38 -0700

    Time: 26 ms (30.84%) | Memory: 11.5 MB (10.13%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int n = s.size(), m = t.size();
-        unordered_map<char, int> hs,ht;         //分别记录s，t中各个字符出现的次数
-        for (auto c : t) ht[c] ++;
-
-        //双指针
-        int cnt = 0;
+        int n = s.size();
+        int cnt = 0;                //s中有效字符数量
         int res = INT_MAX;
         string ans = "";
-        for (int i = 0, j = 0; i < n; i++) {
-            hs[s[i]] ++;
+        unordered_map<char, int> hs, ht;
+        for (auto c : t) ht[c]++;
+        for (int i = 0, j = 0; i < s.size(); i++){
+            hs[s[i]]++;
             if (hs[s[i]] <= ht[s[i]]){
-                //说明当前这个字符是窗口里包含t的必要条件
+                //说明刚刚加入了一个有效字符，
                 cnt++;
             }
-            while (j < n && hs[s[j]] > ht[s[j]]) {
-                //如果当前sj是非必要的左边指针可以右移
+
+            while (j < i && hs[s[j]] > ht[s[j]]){
                 hs[s[j]]--;
                 j++;
             }
-            if (cnt == t.size() && i-j+1 < res) {
+
+            if (cnt == t.size() && i-j+1 < res){
                 res = i-j+1;
                 ans = s.substr(j, res);
             }
