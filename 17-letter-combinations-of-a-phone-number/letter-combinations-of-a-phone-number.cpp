870d586f7481b6a9009b825fe69dde9b0a6df1a8
Junxi (Eric) Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-10-28 13:11:12 -0700

    Time: 0 ms (100.00%) | Memory: 8.8 MB (19.97%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    vector<string> ans;
     vector<string> hash={"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    vector<string> res;
     string cur = "";
-    void dfs(string digits, int idx){
-        if (idx == digits.size()){
-            ans.push_back(cur);
+    void dfs(string s, int t) {
+        if (t == s.size()){
+            res.push_back(cur);
             return;
         }
-        char c = digits[idx];
-        int p = c - '0';
-        for (int i = 0; i < hash[p].size(); i++){
-            cur += hash[p][i];
-            dfs(digits, idx+1);
+
+        auto c = s[t] - '0';
+        for (auto cr : hash[c]){
+            cur += cr;
+            dfs(s, t+1);
             cur = cur.substr(0,cur.size()-1);
         }
     }
+
     vector<string> letterCombinations(string digits) {
         if (digits.size() == 0) return {};
         dfs(digits, 0);
-        return ans;
+        return res;
     }
 };
