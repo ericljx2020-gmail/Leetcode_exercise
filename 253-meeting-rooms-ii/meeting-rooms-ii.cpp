43e85070d1e67e4ad2ccc8f013d1ac5428dee650
Junxi (Eric) Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-10-30 22:42:59 -0700

    Time: 152 ms (5.71%) | Memory: 302.1 MB (5.07%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.cpp
+++ 253-meeting-rooms-ii/meeting-rooms-ii.cpp
@@ -4,12 +4,14 @@ public:
 
     int minMeetingRooms(vector<vector<int>>& intervals) {
         vector<int> f(N, 0);
+        int n = 0;
         for (auto c : intervals) {
             f[c[0]+1]++;
             f[c[1]+1]--;
+            n = max(n, c[1]+1);
         }
         int res = 0;
-        for (int i = 1; i <= 1e6; i++){
+        for (int i = 1; i <= n; i++){
             f[i] += f[i-1];
             res = max(res, f[i]);
         } 
