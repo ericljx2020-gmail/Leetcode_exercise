0e73190f4dc5902fbfbdcb35638d612e310d3049
Junxi (Eric) Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-10-30 22:26:13 -0700

    Time: 2 ms (6.05%) | Memory: 9.6 MB (45.04%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -2,12 +2,14 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        nums.insert(nums.begin(), 0);
-        vector<vector<int>> f(n+1, vector<int> (2, 0));
-        for (int i = 1; i <= n; i++){
-            f[i][0] = max(f[i-1][1], f[i-1][0]);
-            f[i][1] = max(f[i-1][0]+nums[i], f[i-1][1]);
+        vector<vector<int>> f(n, vector<int> (2, 0));
+        f[0][0] = 0;
+        f[0][1] = nums[0];
+
+        for (int i = 1; i < n; i++){
+            f[i][0] = max(f[i-1][0], f[i-1][1]);
+            f[i][1] = f[i-1][0]+nums[i];
         }
-        return max(f[n][1], f[n][0]);
+        return max(f[n-1][0], f[n-1][1]);
     }
 };
