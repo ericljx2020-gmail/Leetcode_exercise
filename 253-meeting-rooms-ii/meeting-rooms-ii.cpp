2857396430b805764f532563cd003eda123619e9
Junxi (Eric) Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-10-30 22:49:06 -0700

    Time: 67 ms (5.71%) | Memory: 33.1 MB (5.07%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.cpp
+++ 253-meeting-rooms-ii/meeting-rooms-ii.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    const int N = 1e6+6;
-
+    static bool cmp(vector<int> a, vector<int> b){
+        return a[0] < b[0];
+    }
     int minMeetingRooms(vector<vector<int>>& intervals) {
-        vector<int> f(N, 0);
-        int n = 0;
+        sort(intervals.begin(), intervals.end(), cmp);
+        priority_queue<int, vector<int>, greater<int>> pq;
+
         for (auto c : intervals) {
-            f[c[0]+1]++;
-            f[c[1]+1]--;
-            n = max(n, c[1]+1);
+            if (pq.size() && c[0] >= pq.top()){
+                pq.pop();
+            }
+
+            pq.push(c[1]);
         }
-        int res = 0;
-        for (int i = 1; i <= n; i++){
-            f[i] += f[i-1];
-            res = max(res, f[i]);
-        } 
-        return res;
+        return pq.size();
     }
 };
