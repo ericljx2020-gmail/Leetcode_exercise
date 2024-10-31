580c84e8588825448c33cc6fbab9442e37ee8b7e
Junxi (Eric) Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-10-30 18:11:05 -0700

    Time: 25 ms (32.78%) | Memory: 11.5 MB (10.05%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int n = s.size();
-        int cnt = 0;                //s中有效字符数量
-        int res = INT_MAX;
-        string ans = "";
         unordered_map<char, int> hs, ht;
+        int cnt = 0;
+        int res = INT_MAX;
         for (auto c : t) ht[c]++;
+        string word = "";
         for (int i = 0, j = 0; i < s.size(); i++){
             hs[s[i]]++;
             if (hs[s[i]] <= ht[s[i]]){
-                //说明刚刚加入了一个有效字符，
                 cnt++;
             }
 
@@ -19,11 +17,13 @@ public:
                 j++;
             }
 
-            if (cnt == t.size() && i-j+1 < res){
-                res = i-j+1;
-                ans = s.substr(j, res);
+            if (cnt == t.size()){
+                if (i-j+1 < res){
+                    res = i-j+1;
+                    word = s.substr(j, res);
+                }
             }
         }
-        return ans;
+        return word;
     }
 };
