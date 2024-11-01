52f7669174f63fadc711e5b0fcaddcdc1beee32d
Junxi (Eric) Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-11-01 12:12:20 -0700

    Time: 0 ms (100.00%) | Memory: 7.5 MB (72.05%) - LeetSync

--- 670-maximum-swap/maximum-swap.cpp
+++ 670-maximum-swap/maximum-swap.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int maximumSwap(int num) {
-        string s = to_string(num);
-        for (int i = 0; i < s.size(); i++){
+        string cp = to_string(num);
+        for (int i = 0; i < cp.size(); i++){
+            char maxv = cp[i]+1;
             int p = -1;
-            char maxv = s[i]+1;
-            for (int j = i+1; j < s.size(); j++){
-                if (s[j] > s[i]){
-                    if (s[j] >= maxv){
-                        maxv = s[j];
-                        p = j;
-                    }
+            for (int j = i+1; j < cp.size(); j++){
+                if (cp[j] >= maxv){
+                    maxv = cp[j];
+                    p = j;
                 }
             }
-            if (p == -1) continue;
-            swap(s[i], s[p]);
-            return stoi(s);
+            if (p != -1){
+                swap(cp[i], cp[p]);
+                return stoi(cp);
+            }
         }
         return num;
     }
