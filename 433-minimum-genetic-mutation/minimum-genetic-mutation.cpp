4e6a3e1be0998f83c72ba7ab967486fec14f5ab8
Junxi (Eric) Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-11-01 14:47:22 -0700

    Time: 0 ms (100.00%) | Memory: 8.4 MB (36.78%) - LeetSync

--- 433-minimum-genetic-mutation/minimum-genetic-mutation.cpp
+++ 433-minimum-genetic-mutation/minimum-genetic-mutation.cpp
@@ -1,35 +1,40 @@
 class Solution {
 public:
 
-    bool check(string a, string b){
-        int n = a.size();
+    bool check(string s1, string s2){
         int cnt = 0;
-        for (int i = 0; i < n; i++){
-            if (a[i] != b[i]) cnt++;
+        for (int i = 0; i < s1.size(); i++){
+            if (s1[i] != s2[i]){
+                cnt++;
+                if (cnt > 1) return false;
+            }
         }
-        return cnt == 1;
+        return true;
     }
 
-    int minMutation(string s, string e, vector<string>& bank) {
-        int n = bank.size();
-        unordered_map<string, int> hash;
+    int minMutation(string startGene, string endGene, vector<string>& words) {
         queue<pair<string, int>> q;
-        q.push({s,0});
-        hash[s] = 1;
+        q.push({startGene, 0});
+        unordered_map<string, bool> hash;
+        int n = words.size();
         while (q.size()){
-            pair<string, int> t = q.front();
+            auto t = q.front().first;
+            auto step = q.front().second;
             q.pop();
+            
             for (int i = 0; i < n; i++){
-                string m = bank[i];
-                if (hash[m]) continue;
-                if (check(t.first, m)){
-                    if (m == e) return t.second+1;
-                    q.push({m, t.second+1});
-                    hash[m] = 1;
+                string word = words[i];
+                if (hash[word]) continue;
+                if (check(word, t)){
+                    if (word == endGene) {
+                        // cout << ;
+                        return step+1;
+                    }
+                    hash[word] = 1;
+                    q.push({word, step+1});
                 }
             }
         }
-
         return -1;
     }
 };
