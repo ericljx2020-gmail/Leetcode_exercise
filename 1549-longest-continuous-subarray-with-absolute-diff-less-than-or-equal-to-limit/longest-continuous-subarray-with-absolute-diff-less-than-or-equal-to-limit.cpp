7af5e4e8ff9737b93ca35bed7e2633bc55abe5b1
Junxi (Eric) Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-10-31 21:24:29 -0700

    Time: 43 ms (60.70%) | Memory: 72.6 MB (96.07%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums, int limit) {
-        multiset<int> ss;       //平衡树，插入删除查找都是O(logn)
         int n = nums.size();
+        deque<int> q1, q2;
         int res = 0;
         for (int i = 0, j = 0; i < n; i++){
-            ss.insert(nums[i]);
-            while (*ss.rbegin() - *ss.begin() > limit){
-                ss.erase(ss.find(nums[j++]));
+            while (q1.size() && nums[i] >= nums[q1.back()]){
+                q1.pop_back();
+            }
+            q1.push_back(i);
+            while (q2.size() && nums[i] <= nums[q2.back()]){
+                q2.pop_back();
+            }
+            q2.push_back(i);
+
+            while (q1.size() && q2.size() && nums[q1.front()] - nums[q2.front()] > limit){
+                if (j == q1.front()){
+                    q1.pop_front();
+                }
+                if (j == q2.front()){
+                    q2.pop_front();
+                }
+                j++;
             }
             res = max(res, i-j+1);
         }
         return res;
+        
+        return 0;
     }
 };
