22fb60645b4b8b546d5230cf16d0a4e2bb06c41e
Junxi (Eric) Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-11-01 12:40:39 -0700

    Time: 43 ms (90.29%) | Memory: 27.3 MB (89.19%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> res;
+        int n = nums.size();
         sort(nums.begin(), nums.end());
-        for (int i = 0; i < nums.size(); i++){
-            if (i > 0 && nums[i] == nums[i-1]) continue;
-            for (int j = i+1, k = nums.size()-1; j < k; j++) {
-                if (j > i+1 && nums[j] == nums[j-1]) continue;
-                while (k > j && nums[i] + nums[j] + nums[k] > 0) k--;
-                if (k != j && nums[i] + nums[j] + nums[k] == 0) res.push_back({nums[i], nums[j], nums[k]});
+        vector<vector<int>> res;
+        for (int i = 0; i < n; i++){
+            if (i && nums[i] == nums[i-1]) continue;
+            for (int j = i+1, k = n-1; j < k; j++){
+                if (j != i+1 && nums[j] == nums[j-1]) continue;
+                while (k > j && nums[i] + nums[j] + nums[k] > 0){
+                    k--;
+                }
+                if (k != j && nums[i] + nums[j] + nums[k] == 0){
+                    res.push_back({nums[i], nums[j], nums[k]});
+                }
             }
         }
         return res;
