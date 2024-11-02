2be5ccc81f0255c09e8cc2cce77ec93bb1fa60d5
Junxi (Eric) Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-11-02 13:59:25 -0700

    Time: 0 ms (100.00%) | Memory: 11.5 MB (63.92%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -3,7 +3,7 @@ public:
     int findPeakElement(vector<int>& nums) {
         int l = 0, r = nums.size()-1;
         while (l < r){
-            int mid = l + r + 1 >> 1;
+            int mid = l + (r-l) + 1 / 2;
             if (nums[mid-1] <= nums[mid]){
                 l = mid;
             }else{
