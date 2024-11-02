2f4664c94fa2190e357b2a96e3fffe8dfc5af703
Junxi (Eric) Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-11-02 14:39:20 -0700

    Time: 9 ms (59.47%) | Memory: 13.1 MB (64.99%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
@@ -2,28 +2,30 @@ class Solution {
 public:
     string minRemoveToMakeValid(string s) {
         int n = s.size();
-        stack<int> stk;
-        string s_copy = "";
-        unordered_map<int, bool> st;
+        stack<char> stk;
+        int cnt = 0;
         for (int i = 0; i < n; i++){
-            if (s[i] <= 'z' && s[i] >= 'a') continue;
-            else if (s[i] == '(') stk.push(i);
-            else{
-                if (stk.empty()) st[i] = 1;             //不要
-                else{
-                    stk.pop();
+            if (s[i] != ')'){
+                stk.push(s[i]);
+                if (s[i] == '(') cnt++;
+            }else{
+                if (cnt > 0) {
+                    stk.push(s[i]);
+                    cnt--;
                 }
             }
         }
-        while (stk.size()){
-            int t = stk.top();
-            st[t] = 1;
+        string res = "";
+        while(stk.size()){
+            auto t = stk.top();
             stk.pop();
+            if (t == '(' && cnt > 0){
+                cnt--;
+                continue;
+            }
+            res += t;
         }
-        for (int i = 0; i < n; i++){
-            if (st[i]) continue;
-            s_copy += s[i];
-        }
-        return s_copy;
+        reverse(res.begin(), res.end());
+        return res;
     }
 };
