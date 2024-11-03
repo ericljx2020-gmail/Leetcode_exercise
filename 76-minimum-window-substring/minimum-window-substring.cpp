5b745f19757b2690c22862b597d53ee381cdd478
Junxi (Eric) Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-11-02 23:38:42 -0700

    Time: 23 ms (38.99%) | Memory: 11.7 MB (7.38%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -3,27 +3,28 @@ public:
     string minWindow(string s, string t) {
         unordered_map<char, int> hs, ht;
         int cnt = 0;
-        int res = INT_MAX;
+
         for (auto c : t) ht[c]++;
-        string word = "";
-        for (int i = 0, j = 0; i < s.size(); i++){
+        int ll = INT_MAX;
+        string res = "";
+        for (int i = 0, j = 0; i < s.size(); i++) {
             hs[s[i]]++;
+
             if (hs[s[i]] <= ht[s[i]]){
-                cnt++;
+                cnt++;      //有效
             }
 
             while (j < i && hs[s[j]] > ht[s[j]]){
                 hs[s[j]]--;
                 j++;
             }
-
-            if (cnt == t.size()){
-                if (i-j+1 < res){
-                    res = i-j+1;
-                    word = s.substr(j, res);
+            if (cnt == t.size()) {
+                if (i-j+1 < ll) {
+                    ll = i-j+1;
+                    res = s.substr(j, i-j+1);
                 }
             }
         }
-        return word;
+        return res;
     }
 };
