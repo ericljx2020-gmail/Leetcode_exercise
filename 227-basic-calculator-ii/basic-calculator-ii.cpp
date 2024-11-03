0e7febf72cd05f585a87ffc3d0853ef4a4ace928
Junxi (Eric) Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-11-03 12:01:21 -0800

    Time: 27 ms (17.54%) | Memory: 14.7 MB (23.42%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.cpp
+++ 227-basic-calculator-ii/basic-calculator-ii.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    stack<int> nums;
+    unordered_map<char, int> pr;
     stack<char> op;
+    stack<int> nums;
     int calculate(string s) {
-        unordered_map<char, int> pr;
-        pr['+'] = pr['-'] = 1, pr['*'] = pr['/'] = 2;
         int n = s.size();
+        pr['+'] = pr['-'] = 1, pr['*'] = pr['/'] = 2;
         for (int i = 0; i < n; i++){
             if (s[i] == ' ') continue;
-            if (s[i] == '(') op.push(s[i]);
-            else if (s[i] == ')'){
+            else if (s[i] == '(') {
+                op.push(s[i]);
+            }else if (s[i] == ')'){
                 while (op.size() && op.top() != '('){
                     eval();
                 }
             }else if (isdigit(s[i])){
                 int j = i;
                 int cur = 0;
-                while (j < n && isdigit(s[j])){
+                while (j < s.size() && isdigit(s[j])){
                     cur = cur * 10 + (s[j] - '0');
                     j++;
                 }
@@ -31,20 +32,19 @@ public:
         }
         while (op.size()){
             eval();
-            // cout << op.top() << " ";
-            // op.pop();
         }
         return nums.top();
     }
 
-    void eval() {
-        auto o = op.top(); op.pop();
+    void eval(){
+        char o = op.top(); op.pop();
         int b = nums.top(); nums.pop();
         int a = nums.top(); nums.pop();
+
         if (o == '+') nums.push(a+b);
         if (o == '-') nums.push(a-b);
         if (o == '*') nums.push(a*b);
-        if (o == '/') nums.push(a / b);
+        if (o == '/') nums.push(a/b);
     }
 
 };
