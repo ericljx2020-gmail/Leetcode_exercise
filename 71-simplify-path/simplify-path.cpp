95600def018744ac58c7befbfee5ae04cfbb5d91
Junxi (Eric) Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-11-03 17:48:37 -0800

    Time: 7 ms (16.19%) | Memory: 13 MB (7.95%) - LeetSync

--- 71-simplify-path/simplify-path.cpp
+++ 71-simplify-path/simplify-path.cpp
@@ -1,32 +1,38 @@
 class Solution {
 public:
     string simplifyPath(string path) {
+        string cur = "";
         int n = path.size();
-        string s = "";
-        string q[3005];
-        int tt = -1;
+        vector<string> dir;
         for (int i = 0; i < n; i++){
-            if (path[i] == '/' || i == n-1){
-                if (i == n-1 && path[i] != '/') s += path[i];
-                if (s == ".."){
-                    tt--;
-                    tt = max(-1,tt);
-                }else if (s == "." || s == ""){
-                    s = "";
-                    continue;
-                }else{
-                    q[++tt] = s;
-                }
-                s = "";
+            if (path[i] == '/'){
+                dir.push_back(cur);
+                cur = "";
             }else{
-                s += path[i];
+                cur += path[i];
             }
         }
-        string res = "/";
-        for (int i = 0; i <= tt; i++) {
-            if (i != 0) res += '/';
-            res += q[i];
+        dir.push_back(cur);
+        stack<string> stk;
+        for (auto d : dir) {
+            if (d == ".."){
+                if (stk.size())stk.pop();
+            }else if (d != "." && d != ""){
+                stk.push(d);
+            }
         }
-        return res;
+        vector<string> reversed_dir;
+
+        string res = "/";
+        while (stk.size()){
+            reversed_dir.push_back(stk.top());
+            stk.pop();
+        }
+
+        reverse(reversed_dir.begin(), reversed_dir.end());
+        for (auto d : reversed_dir){
+            res += d + '/';
+        }
+        return res == "/" ? res : res.substr(0, res.size()-1);
     }
 };
