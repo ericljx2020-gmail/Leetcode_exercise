b3a394a0226df504a8c66fad7ff3b7f89194d1b8
Junxi (Eric) Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-11-03 22:23:39 -0800

    Time: 7 ms (86.95%) | Memory: 21.3 MB (30.50%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
+    vector<vector<int>> f, w;
     int n,m;
-    unordered_map<int, unordered_map<int, int>> cache;
     int dx[4] = {0,0,-1,1};
     int dy[4] = {1,-1,0,0};
-    vector<vector<int>> matrix;
-    int longestIncreasingPath(vector<vector<int>>& _matrix) {
-        matrix = _matrix;
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        if (matrix.empty() || matrix[0].empty()) return 0;
         n = matrix.size(), m = matrix[0].size();
+        w = matrix;
+        f = vector(n, vector<int> (m, -1));
         int res = 0;
         for (int i = 0; i < n; i++){
             for (int j = 0; j < m; j++){
-                if (!cache[i][j]){
-                    dfs(i,j);
-                }
-                res = max(res, cache[i][j]);
+                res = max(res, dp(i,j));
             }
         }
         return res;
     }
 
-    int dfs(int x, int y){
-        if (cache[x][y]) return cache[x][y];
-        int res = 0;
+    int dp(int x, int y) {
+        auto &v = f[x][y];
+        if (v != -1) return v;
+
+        v = 1;
+
         for (int i = 0; i < 4; i++){
             int a = x + dx[i];
             int b = y + dy[i];
-            if (a < 0 || b < 0 || a >= n || b >= m) continue;
-            if (matrix[a][b] <= matrix[x][y]) continue;
-            res = max(res, dfs(a,b));
+            if (a < 0 || a >= n || b < 0 || b >= m) continue;
+            if (w[a][b] > w[x][y]) {
+                v = max(v, dp(a,b)+1);
+            }
         }
-        res += 1;           //加上自己的长度
-        cache[x][y] = res;
-        return res;
+        return v;
+        
     }
 
 };
