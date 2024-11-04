f266ec52bdc170e69d1006e1492105d0a1b2dd1b
Junxi (Eric) Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-11-03 17:29:23 -0800

    Time: 4 ms (51.69%) | Memory: 17.8 MB (17.78%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
-
-    static bool cmp(int a, int b) {return a > b;}
-
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int> hash1;
-        unordered_map<int, vector<int>> hash2;
-        for(auto c : nums) hash1[c]++;
-        vector<int>f;
-        for (auto t : hash1) {
-            hash2[t.second].push_back(t.first);
+        unordered_map<int, int> cnt;
+        int n = nums.size();
+        for (auto c : nums) cnt[c]++;
+        vector<int> s(n+1, 0);
+        for (auto [k, v] : cnt){
+            s[v]++;
         }
-        for (auto t : hash2){
-            f.push_back(t.first);
+        int i = n, t = 0;
+        while (i >= 0 && t < k){
+            t += s[i--];
         }
-        sort(f.begin(), f.end(), cmp);
-        int res = 0;
-        vector<int> ans;
-        for (int i = 0; i < f.size(); i++){
-            for (auto c : hash2[f[i]]){
-                ans.push_back(c);
-                res++;
-                if (res == k) return ans;
+        vector<int> res;
+        for (auto [k,v] : cnt){
+            if (v > i){
+                res.push_back(k);
             }
         }
-        return {};
+        return res;
     }
 };
