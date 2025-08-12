136b6f5e204fda37d722257d090f9a7b0b5bd7ef
Junxi (Eric) Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2025-08-11 20:11:23 -0700

    Time: 314 ms (71.80%) | Memory: 18 MB (62.50%) - LeetSync

--- 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.py
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.py
@@ -1,18 +1,15 @@
 class Solution:
     def numberOfWays(self, n: int, x: int) -> int:
-        MOD = 1e9+7
-        powers = []
-        b = 1
-        while b < 400:
-            a = pow(b, x)
-            if a > n: break
-            powers.append(a)
-            b += 1
-
         f = [0 for _ in range(n+1)]
         f[0] = 1
-        for power in powers:
-            for i in range(n, -1, -1):
-                if i >= power: f[i] =  (f[i] + f[i-power]) % MOD
+        MOD = 1e9+7
+        t = 0
+        while pow(t+1,x) <= n:
+            t += 1
+        
+        for i in range(1,t+1):
+            d = pow(i,x)
+            for j in range(n, d-1, -1):
+                f[j] = (f[j] + f[j-d]) % MOD;
 
-        return int(f[n] % MOD)
+        return int(f[n])
