12697a3a55ae70bbab9af781839899c81c8c72a6
Junxi (Eric) Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2025-08-25 22:31:22 -0700

    Time: 0 ms (100.00%) | Memory: 31 MB (6.00%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -2,16 +2,12 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
-        unordered_map<int,int> hash;
-        for (int i = 0; i < n; i++) {
-            hash[i] = (i+k) % n;
+        k %= n;
+        vector<int> nxt;
+        for (int i = n-k; i < 2*n-k; i++){
+            nxt.push_back(nums[i%n]);
         }
-        vector<int> res(n,0);
-        for(int i = 0; i < n; i++){
-            res[hash[i]] = nums[i];
-            // cout << hash[nums[i]] << " ";
-        }
-        nums = res;
+        nums = nxt;
         return;
     }
 };
