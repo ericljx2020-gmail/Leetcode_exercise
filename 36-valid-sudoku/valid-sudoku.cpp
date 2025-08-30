bedba1a68bf20b746f0c19881264d4f9cc90d35d
Junxi (Eric) Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2025-08-29 22:52:05 -0700

    Time: 5 ms (43.86%) | Memory: 25.4 MB (35.57%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,18 +1,46 @@
 class Solution {
 public:
+    int twoDimensionToOne(int x, int y){
+        return 3*x+y;
+    }
     bool isValidSudoku(vector<vector<char>>& board) {
-        int n = board.size();
-        unordered_map<int, unordered_map<int, bool>> rol, col, g;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        unordered_map<int, unordered_set<int>> row, col, grid;
+        // process row
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++){
                 if (board[i][j] == '.') continue;
-                int cur = board[i][j] - '0';
-                int oneD = i / 3 * 3 + j / 3;
-                if (g[oneD][cur] || rol[i][cur] || col[j][cur]) {
-                    return false; 
+                int c = board[i][j] - '0';
+                if (row[i].find(c) == row[i].end()){
+                    row[i].insert(c);
+                }else{
+                    return false;
                 }
-                g[oneD][cur] = rol[i][cur] = col[j][cur] = 1;
+            }
+        }
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++){
+                if (board[j][i] == '.') continue;
+                int c = board[j][i] - '0';
+                if (col[i].find(c) == col[i].end()){
+                    col[i].insert(c);
+                }else{
+                    return false;
+                }
+            }
+        }
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') continue;
+                int x = i / 3;
+                int y = j / 3;
+                int pos = twoDimensionToOne(x,y);
+                int c = board[i][j] - '0';
+                if (grid[pos].find(c) == grid[pos].end()) {
+                    grid[pos].insert(c);
+                }else{
+                    return false;
+                }
             }
         }
         return true;
