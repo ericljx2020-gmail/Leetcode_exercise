d6f427f7b33c1db4615a505ef63fa07b688a6c39
Junxi (Eric) Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2025-09-03 11:33:51 -0700

    Time: 3 ms (14.09%) | Memory: 13.2 MB (9.45%) - LeetSync

--- 274-h-index/h-index.cpp
+++ 274-h-index/h-index.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
-    static bool cmp(int a, int b) {
-        return a > b;
-    }
-
-    int hIndex(vector<int>& citations) {
-        int n = citations.size();
-        sort(citations.begin(), citations.end(), cmp);
-        citations.insert(citations.begin(), 0);
-        for (int i = 1; i <= n; i++){
-            if (i > citations[i]) return i-1;
+    bool check(int h, vector<int> citations) {
+        int cnt = 0;
+        for (auto c : citations) {
+            if (c >= h) cnt++;
         }
-        return n;
+        return cnt >= h;
+    }
+    int hIndex(vector<int>& citations) {
+        int l = 0, r = citations.size();
+        while (l < r){
+            int mid = l + r + 1 >> 1;
+            if (check(mid, citations)){
+                l = mid;
+            }else{
+                r = mid-1;
+            }
+        }
+        return r;
     }
 };
