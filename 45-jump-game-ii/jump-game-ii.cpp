b04f3578b6c22a156830509b8e42054abafd2051
Junxi (Eric) Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2025-09-03 11:22:45 -0700

    Time: 495 ms (5.01%) | Memory: 21.5 MB (27.04%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,30 +1,15 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        if (nums.size() == 1) return 0;
         int n = nums.size();
-        int r = 0;
-        int step = 0;
-        int curm = 0;
+        vector<int> f(n, INT_MAX);        // f[i]: minimum # step to jump to i
+        f[0] = 0;
         for (int i = 0; i < n; i++){
-            if (i == 0) r = nums[i];
-            int j = i+1;
-            int p = i;
-            curm = max(curm, i+nums[i]);
-            if (curm >= n-1) return step+1;
-            while (j <= r && j < n){
-                if (j+nums[j] > curm){
-                    curm = j + nums[j];
-                    p = j;
-                }
-                j++;
+            for (int j = 0; j < i; j++){
+                if (j+nums[j] >= i) f[i] = min(f[i], f[j]+1);
             }
-            cout << p << " " << curm << '\n';
-            step++;
-            i = p-1;
-            r = curm;
-            if (r >= n-1) return step+1;
         }
-        return 0;
+        return f[n-1];
+
     }
 };
