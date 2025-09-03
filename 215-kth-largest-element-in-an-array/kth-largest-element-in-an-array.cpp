e60b0dbc46b32e609e0d1cf77a0f2a4530988cf6
Junxi (Eric) Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2025-09-03 14:20:26 -0700

    Time: 72 ms (5.59%) | Memory: 243.8 MB (10.69%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
-
-    int quicksort(vector<int> nums, int l, int r, int k){
-        if (l == r) return nums[k];
+    int quick_find(vector<int> nums, int l, int r, int k) {
+        if (l == r) return nums[l];
         int x = nums[l], i = l-1, j = r+1;
         while (i < j){
             do i++; while (nums[i] > x);
             do j--; while (nums[j] < x);
             if (i < j) swap(nums[i], nums[j]);
         }
-        if (k <= j) return quicksort(nums, l, j, k);
-        else return quicksort(nums, j+1, r, k);
+        if (j < k) return quick_find(nums, j+1, r, k);
+        return quick_find(nums, l,j,k);
     }
-
     int findKthLargest(vector<int>& nums, int k) {
-        return quicksort(nums, 0, nums.size()-1, k-1);
+        return quick_find(nums, 0, nums.size()-1, k-1);
     }
 };
