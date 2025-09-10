e1d30dc47363a23818177969a1bb0e42af89d3e2
Junxi (Eric) Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2025-09-10 14:25:15 -0700

    Time: 6 ms (5.85%) | Memory: 26.9 MB (44.79%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,45 +1,21 @@
 class Solution {
 public:
-    int trap(vector<int>& a) {
-        int n = a.size();
-        vector<int> r(n,n), l(n,-1);
-        int q[n+1];
-        int tt = -1;
-        for (int i = 0; i < n; i++){
-            while (tt != -1 && a[q[tt]] <= a[i]){
-                r[q[tt--]] = i;
-            }
-            q[++tt] = i;
-        }
-        tt = -1;
-        for (int i = n-1; i >= 0; i--){
-            while (tt != -1 && a[q[tt]] <= a[i]) {
-                l[q[tt--]] = i;
-            }
-            q[++tt] = i;
-        }
-        // for (int i = 0; i < n; i++) cout << r[i] << " ";
-        // puts("");
-        // for (int i = 0; i < n; i++) cout << l[i] << " ";
+    int trap(vector<int>& height) {
+        int n = height.size();
+        stack<int> stk;
         int res = 0;
         for (int i = 0; i < n; i++){
-            if (r[i] != n){
-                for (int j = i+1; j < r[i]; j++) {
-                    res += max(0,a[i]-a[j]);
-                    a[j] = a[i];
-                }
+            int last = 0;
+            while (stk.size() && height[stk.top()] <= height[i]){
+                int j = stk.top();
+                stk.pop();
+                int h = height[j];
+                res += (i-j-1) * (h-last);
+                last = h;
             }
+            if (stk.size()) res += (i-stk.top()-1) * (height[i] - last);
+            stk.push(i);
         }
-        // cout << res << endl;
-        for (int i = n-1; i >= 0; i--){
-            if (l[i] != -1){
-                for (int j = i-1; j > l[i]; j--){
-                    res += max(0, a[i]-a[j]);
-                    a[j] = a[i];
-                }
-            }
-        }
-
         return res;
     }
 };
