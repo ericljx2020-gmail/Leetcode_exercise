b0be0f988372738fb6f4611530c6781ba1d226e5
Junxi (Eric) Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2025-09-10 14:31:35 -0700

    Time: 4 ms (10.69%) | Memory: 27.1 MB (39.71%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -2,19 +2,16 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        stack<int> stk;
+        stack<int>stk;
         int res = 0;
-        //维护一个从高到低的栈，每次新进来一个元素如果大于等于栈顶，处理完保证从i到stk.top之间的高度都是height[stk.top()]
         for (int i = 0; i < n; i++){
             int last = 0;
             while (stk.size() && height[stk.top()] <= height[i]){
-                int j = stk.top();
+                res += (i-stk.top()-1) * (height[stk.top()] - last);
+                last = height[stk.top()];
                 stk.pop();
-                int h = height[j];
-                res += (i-j-1) * (h-last);
-                last = h;
             }
-            if (stk.size()) res += (i-stk.top()-1) * (height[i] - last);
+            if (stk.size()) res += (i - stk.top()-1) * (height[i] - last);
             stk.push(i);
         }
         return res;
