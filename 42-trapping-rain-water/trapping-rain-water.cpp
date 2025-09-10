289f49daebbb62f12410a193602d7638413611af
Junxi (Eric) Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2025-09-10 14:27:13 -0700

    Time: 6 ms (5.85%) | Memory: 26.8 MB (44.79%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -4,6 +4,7 @@ public:
         int n = height.size();
         stack<int> stk;
         int res = 0;
+        //维护一个从高到低的栈，每次新进来一个元素如果大于等于栈顶，处理完保证从i到stk.top之间的高度都是height[stk.top()]
         for (int i = 0; i < n; i++){
             int last = 0;
             while (stk.size() && height[stk.top()] <= height[i]){
