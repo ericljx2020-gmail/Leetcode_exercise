fc1795becab040aa483c993c10f4f9237561ad10
Junxi (Eric) Li <68884486+ericljx2020-gmail@users.noreply.github.com>
2025-09-16 16:19:51 -0400

    Time: 11 ms (51.23%) | Memory: 12.4 MB (5.13%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
+        int n = s.size(), m = t.size();
         unordered_map<char, int> hs, ht;
-        int cnt = 0;
-
         for (auto c : t) ht[c]++;
-        int ll = INT_MAX;
-        string res = "";
-        for (int i = 0, j = 0; i < s.size(); i++) {
+        int res = INT_MAX;
+        string ans = "";
+        int cnt = 0;
+        for (int i = 0, j = 0; i < n; i++){
             hs[s[i]]++;
-
             if (hs[s[i]] <= ht[s[i]]){
-                cnt++;      //有效
+                cnt++;
             }
 
             while (j < i && hs[s[j]] > ht[s[j]]){
                 hs[s[j]]--;
                 j++;
             }
-            if (cnt == t.size()) {
-                if (i-j+1 < ll) {
-                    ll = i-j+1;
-                    res = s.substr(j, i-j+1);
-                }
+            if (cnt == t.size() && i-j+1 < res){
+                res = i-j+1;
+                ans = s.substr(j, i-j+1);
             }
         }
-        return res;
+        return ans;
     }
 };
